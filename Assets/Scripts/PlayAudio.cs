18fb5c17a78daeab01b34e7b4e872d50161dfca0
srgovindan <22505760+srgovindan@users.noreply.github.com>
2025-12-24 16:27:46 +0530

    create timer and audio script

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SimpleTimer : MonoBehaviour
+{
+    public float timerDuration = 2f;
+    private float timer= 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gives us the time since the start of the game running
+        //Debug.Log(Time.time);
+
+        if (timer < timerDuration)
+        {
+            timer = timer + Time.deltaTime;
+            Debug.Log(timer);
+        }
+        else
+        {
+            //the timer duration has passed 
+            //do something else 
+            Debug.Log("DO COOL TIMED STUFF.");
+            timer = 0;
+        }
+        
+    }
+}
