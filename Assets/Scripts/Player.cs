fe51caecd91e91974c3655e0b4e0ced9df9a5762
srgovindan <22505760+srgovindan@users.noreply.github.com>
2025-12-31 11:51:02 +0530

    created a confetti particle system that plays when the player has more than 20 points

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public ParticleSystem confettiParticleSystem;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //call the function from the GameManager
+            //the GM is a singleton so we can access it like this
             GameManager.instance.ChangePoints(7);
         }
+        
+        //if the player gets 20 points or more,
+        if (GameManager.instance.points > 20)
+        {
+            //play the confetti particle system
+            confettiParticleSystem.Play();
+        }
+
     }
+    
+    
+    
 }
